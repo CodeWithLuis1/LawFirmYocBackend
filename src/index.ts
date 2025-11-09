e7a27ed5ed3352d2974fe6582752b8c66ec8963b
Luis Gonzalez <codewithluislg@gmail.com>
2025-11-09 13:55:46 -0600

    Fix: usar process.env.PORT y ajustar config para Render

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import server from './server.js'
 import colors from 'colors'
 
 const port = process.env.PORT || 5000
-server.listen(5000,()=>{
-    console.log(colors.bgCyan.bold(`REST API, en el perto ${port}`))
+
+server.listen(port, () => {
+  console.log(colors.bgCyan.bold(`🚀 REST API corriendo en el puerto ${port}`))
 })
